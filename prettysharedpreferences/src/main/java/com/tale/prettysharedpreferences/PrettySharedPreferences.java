fb78053c11130cbbc0ed7670814e24f0114b6a89
tale <giangnguyen.tale@gmail.com>
2014-10-22 21:22:05 +0700

    + Add missing editor for shared preference

--- prettysharedpreferences/src/main/java/com/tale/prettysharedpreferences/PrettySharedPreferences.java
+++ prettysharedpreferences/src/main/java/com/tale/prettysharedpreferences/PrettySharedPreferences.java
@@ -11,35 +11,83 @@ import java.util.Map;
 public class PrettySharedPreferences {
 
     private SharedPreferences sharedPreferences;
-    private Map<String, TypeEditor> typeEditorMap;
+    private static Map<String, TypeEditor> typeEditorMap;
 
     public PrettySharedPreferences(SharedPreferences sharedPreferences) {
         this.sharedPreferences = sharedPreferences;
         typeEditorMap = new Hashtable<String, TypeEditor>();
     }
 
-    public StringEditor getStringEditor(String key, boolean autoCommit) {
+    public StringEditor getStringEditor(String key) {
         TypeEditor typeEditor = typeEditorMap.get(key);
         if (typeEditor == null) {
             typeEditor = new StringEditor(sharedPreferences, key);
             typeEditorMap.put(key, typeEditor);
         } else if (!(typeEditor instanceof StringEditor)) {
-            throw new IllegalArgumentException(String.format("key is already exists for other %s type", typeEditor.getClass().getName()));
+            throw new IllegalArgumentException(String.format("key %s is duplicated", key));
         }
 
         return (StringEditor) typeEditor;
     }
 
-    public LongEditor getLongEditor(String key, boolean autoCommit) {
+    public LongEditor getLongEditor(String key) {
         TypeEditor typeEditor = typeEditorMap.get(key);
         if (typeEditor == null) {
             typeEditor = new LongEditor(sharedPreferences, key);
             typeEditorMap.put(key, typeEditor);
         } else if (!(typeEditor instanceof LongEditor)) {
-            throw new IllegalArgumentException(String.format("key is already exists for other %s type", typeEditor.getClass().getName()));
+            throw new IllegalArgumentException(String.format("key %s is duplicated", key));
         }
 
         return (LongEditor) typeEditor;
+    }
+
+    public IntegerEditor getIntegerEditor(String key) {
+        TypeEditor typeEditor = typeEditorMap.get(key);
+        if (typeEditor == null) {
+            typeEditor = new IntegerEditor(sharedPreferences, key);
+            typeEditorMap.put(key, typeEditor);
+        } else if (!(typeEditor instanceof IntegerEditor)) {
+            throw new IllegalArgumentException(String.format("key %s is duplicated", key));
+        }
+
+        return (IntegerEditor) typeEditor;
+    }
+
+    public BooleanEditor getBooleanEditor(String key) {
+        TypeEditor typeEditor = typeEditorMap.get(key);
+        if (typeEditor == null) {
+            typeEditor = new BooleanEditor(sharedPreferences, key);
+            typeEditorMap.put(key, typeEditor);
+        } else if (!(typeEditor instanceof BooleanEditor)) {
+            throw new IllegalArgumentException(String.format("key %s is duplicated", key));
+        }
+
+        return (BooleanEditor) typeEditor;
+    }
+
+    public FloatEditor getFloatEditor(String key) {
+        TypeEditor typeEditor = typeEditorMap.get(key);
+        if (typeEditor == null) {
+            typeEditor = new FloatEditor(sharedPreferences, key);
+            typeEditorMap.put(key, typeEditor);
+        } else if (!(typeEditor instanceof FloatEditor)) {
+            throw new IllegalArgumentException(String.format("key %s is duplicated", key));
+        }
+
+        return (FloatEditor) typeEditor;
+    }
+
+    public DoubleEditor getDoubleEditor(String key) {
+        TypeEditor typeEditor = typeEditorMap.get(key);
+        if (typeEditor == null) {
+            typeEditor = new DoubleEditor(sharedPreferences, key);
+            typeEditorMap.put(key, typeEditor);
+        } else if (!(typeEditor instanceof DoubleEditor)) {
+            throw new IllegalArgumentException(String.format("key %s is duplicated", key));
+        }
+
+        return (DoubleEditor) typeEditor;
     }
 
 }
